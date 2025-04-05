0443969abb0d7ad834b611882aeaf1433044d74a
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-04-05 10:29:07 -0500

    change GET /cuentas enpoint to return the dto

--- src/main/java/ec/dev/samagua/ntt_data_challenge_accounts/controllers/CuentaController.java
+++ src/main/java/ec/dev/samagua/ntt_data_challenge_accounts/controllers/CuentaController.java
@@ -2,6 +2,7 @@ package ec.dev.samagua.ntt_data_challenge_accounts.controllers;
 
 
 import ec.dev.samagua.ntt_data_challenge_accounts.controllers_models.ControllerResult;
+import ec.dev.samagua.ntt_data_challenge_accounts.dtos.CuentaDto;
 import ec.dev.samagua.ntt_data_challenge_accounts.dtos_mappers.CuentaDtoMapper;
 import ec.dev.samagua.ntt_data_challenge_accounts.entities.Cuenta;
 import ec.dev.samagua.ntt_data_challenge_accounts.services.CuentaService;
@@ -25,13 +26,13 @@ public class CuentaController {
     private final CuentaDtoMapper mapper;
 
     @GetMapping("/cuentas")
-    public Mono<ControllerResult<List<Cuenta>>> findAll() {
+    public Mono<ControllerResult<List<CuentaDto>>> findAll() {
         Mono<ServiceResult<List<Cuenta>>> entities = service.findAll();
         return entities.map(obj -> {
             if (obj.hasErrors()) {
                 return ControllerResult.getErrorResult("1234", "An error has occurred while searching accounts", obj.getErrors());
             }
-            return ControllerResult.getSuccessResult(obj.getValue());
+            return ControllerResult.getSuccessResult(obj.getValue().stream().map(mapper::entityToDto).toList());
         });
     }
 
